662dba1a4010fe2f11312dfe0c8096ad8a318f1c
youseff11 <yo5739240@gmail.com>
2025-06-12 00:53:43 +0300

    Change WSGI entry point to 'app' for Vercel compatibility

--- project/wsgi.py
+++ project/wsgi.py
@@ -4,7 +4,5 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
 
-application = get_wsgi_application()
-
-# أعد إضافة هذا السطر
-handler = application
+# غير 'application' إلى 'app'
+app = get_wsgi_application()
