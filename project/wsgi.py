c25b2ea281804f1c9c494e1418b72245854b5132
youseff11 <yo5739240@gmail.com>
2025-06-12 00:42:01 +0300

    Add 'handler' variable to wsgi.py for Vercel

--- project/wsgi.py
+++ project/wsgi.py
@@ -1,12 +1,3 @@
-"""
-WSGI config for project project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-"""
-
 import os
 
 from django.core.wsgi import get_wsgi_application
@@ -14,3 +5,6 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
 
 application = get_wsgi_application()
+
+# أضف هذا السطر الجديد
+handler = application
