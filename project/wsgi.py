bf1428b6c785ad60a0854d851b325d22c075ab53
youseff11 <yo5739240@gmail.com>
2025-06-12 00:46:17 +0300

    Refactor wsgi.py and vercel.json for Vercel deployment (TypeError fix attempt)

--- project/wsgi.py
+++ project/wsgi.py
@@ -6,5 +6,5 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
 
 application = get_wsgi_application()
 
-# أضف هذا السطر الجديد
-handler = application
+# قم بإزالة السطر التالي:
+# handler = application
