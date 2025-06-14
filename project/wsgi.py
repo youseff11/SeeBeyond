0854c78a4ea82ff95b1116490b5a4386a10af61f
youseff11 <yo5739240@gmail.com>
2025-06-14 14:44:50 +0300

    Remove ignored files from Git tracking

--- project/wsgi.py
+++ project/wsgi.py
@@ -1,12 +1,6 @@
 import os
-
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings') 
 
-# هذا هو متغير WSGI القياسي في Django، ويستخدمه خادم التطوير المحلي (runserver).
 application = get_wsgi_application()
-
-# هذا هو متغير WSGI البديل الذي تتوقعه بعض بيئات النشر مثل Vercel.
-# نقوم بجعله يشير إلى تطبيق Django الرئيسي.
-app = application
