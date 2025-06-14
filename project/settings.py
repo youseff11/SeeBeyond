9075310719f8d340ecaaaca3f8d4b390056e5001
youseff11 <yo5739240@gmail.com>
2025-06-14 15:28:40 +0300

    Remove ignored files from Git tracking

--- project/settings.py
+++ project/settings.py
@@ -1,22 +1,11 @@
 from pathlib import Path
 import os
-import dj_database_url
-from dotenv import load_dotenv 
-
-
-load_dotenv()
-
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# SECRET_KEY = os.environ.get('SECRET_KEY')
-# if not SECRET_KEY:
-#     raise ValueError("SECRET_KEY environment variable not set.")
+DEBUG = True
+
 SECRET_KEY='qsjZEd_RMngZfCervEoKhKFd7RzSiKS5GhVJYCTmH9TR-J8-VmSiyc53LlxrqGaJrOY'
 
-
-# Check if DATABASE_URL is set (indicating a production environment like Heroku)
-IS_PRODUCTION_ENV = os.environ.get('DATABASE_URL') is not None
-DEBUG = not IS_PRODUCTION_ENV # DEBUG is False in production, True in local development
 
 ALLOWED_HOSTS = ['*']
 
@@ -62,25 +51,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'project.wsgi.application'
 
-# Database configuration based on environment
-DATABASE_URL = os.environ.get('DATABASE_URL')
-
-if DATABASE_URL:
-    # Use PostgreSQL provided by Heroku in production
-    DATABASES = {
-        'default': dj_database_url.config(
-            default=DATABASE_URL,
-            conn_max_age=600 # Keep connections alive for 10 minutes
-        )
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
-else:
-    # Use SQLite for local development when DATABASE_URL is not set
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3', # This will create db.sqlite3 in your project root
-        }
-    }
+}
 
 AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
